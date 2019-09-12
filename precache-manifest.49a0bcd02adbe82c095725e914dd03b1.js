1b0a91f86061db717bc0d53dcd7f1b5fd1561e62
hathex <50103127+hathex@users.noreply.github.com>
2019-09-12 09:32:57 +0200

    g

---
+++ precache-manifest.49a0bcd02adbe82c095725e914dd03b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a072f0d682ea2d4d21278e6a886786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bea3e87f7d66ac3989c",
+    "url": "/static/css/main.8453164c.chunk.css"
+  },
+  {
+    "revision": "bca0ddbd1b2f99f07f48",
+    "url": "/static/js/2.f7edbefe.chunk.js"
+  },
+  {
+    "revision": "5bea3e87f7d66ac3989c",
+    "url": "/static/js/main.038ef032.chunk.js"
+  },
+  {
+    "revision": "4ef62094a240c7d569ea",
+    "url": "/static/js/runtime~main.1b7e9b59.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/static/media/pokeball.f10bf078.png"
+  }
+]);
